a0f276cf50020d9b7655fb35f960251a99936368
Glitch (hello-express) <none>
2020-10-06 20:47:16 +0000

    👳🚊 Checkpoint ./Alone.js:18921333/2694

--- Alone.js
+++ Alone.js
@@ -95,7 +95,7 @@ client.on('message' , message => {
     ;
     let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
     if(message.content.startsWith(prefix + 'unban')) {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
+        if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('❌|**\`BAN_MEMBERS\`لا توجد لديك رتبة`**');
         if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
         message.guild.unban(user);
         message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
@@ -2230,6 +2230,50 @@ client.on('message', async message => {
        
     }
 })
+
+
+
+client.on('message', message => {
+  if(!message.channel.guild) return;
+    if (message.content.startsWith(prefix + 'roles')) {
+      if (!message.channel.guild) return;
+ 
+        const Rank = message.guild.roles.map(e => e.toString()).join(" ");
+ 
+        const RankList = new Discord.RichEmbed()
+            .setTitle('➠ Roles.')
+            .setAuthor(message.guild.name, message.guild.iconURL)
+            .setColor('RANDOM')
+            .setDescription(Rank)
+            .setFooter(message.guild.name)
+        message.channel.send(RankList)
+    }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "bans")) {
+      if (!message.channel.guild) return;
+    message.channel
+        message.guild.fetchBans()
+        .then(bans => message.channel.send(`:small_orange_diamond: **Server Ban List :** ${bans.size} `))
+  .catch(console.error);
+}
+});
+
+
+
+var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'animal')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
 
 
 

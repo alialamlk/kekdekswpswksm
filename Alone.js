f05a8a29098979befd4f183d2ea1e26a8b089e47
Glitch (hello-express) <none>
2020-10-06 16:04:22 +0000

    🐾👹 Checkpoint ./Alone.js:18921333/12476

--- Alone.js
+++ Alone.js
@@ -49,7 +49,44 @@ client.on('ready', () => {
 
 //كود البان
 
+client.on("message", message => {
+  if (message.author.codes) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+    if (message.author.bot) return;
+    if (!message.channel.guild)
+      return message.reply("** This command only for servers**");
+
+    if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS"))
+      return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
+    if (!message.guild.member(client.user).hasPermission("BAN_MEMBERS"))
+      return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    let user = message.mentions.users.first();
+
+    if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+    if (
+      message.mentions.members.first().highestRole.position >=
+      message.member.highestRole.position
+    )
+      return message.channel.send("ما تقدر تبند شخص رتبته اعلى منك!");
+    if (!message.guild.member(user).bannable)
+      return message.reply(
+        "**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**"
+      );
+
+    message.guild.member(user).ban(7, user);
+
+    message.channel.send(
+      `**:white_check_mark: ${user.tag} banned from the server ! :airplane: **  `
+    );
+  }
+});
 
 //فك البان
 
@@ -122,6 +159,235 @@ client.on("message", pixelbot => {
     });
   }
 });
+
+
+//كود الكيك
+client.on("message", message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+    if (message.author.bot) return;
+    if (!message.channel.guild)
+      return message.reply("** This command only for servers**");
+
+    if (!message.guild.member(message.author).hasPermission("KICK_MEMBERS"))
+      return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+    if (!message.guild.member(client.user).hasPermission("KICK_MEMBERS"))
+      return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+    let user = message.mentions.users.first();
+    let reason = message.content
+      .split(" ")
+      .slice(2)
+      .join(" ");
+    if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+    if (!reason) return message.reply("**اكتب سبب الطرد**");
+    if (!message.guild.member(user).kickable)
+      return message.reply(
+        "**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**"
+      );
+    if (
+      message.mentions.members.first().highestRole.position >=
+      message.member.highestRole.position
+    )
+      return message.channel.send("ما تقدر تطرد شخص رتبته اعلى منك!");
+
+    message.guild.member(user).kick();
+
+    const kickembed = new Discord.RichEmbed()
+      .setAuthor(`KICKED!`, user.displayAvatarURL)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addField("**User:**", "**[ " + `${user.tag}` + " ]**")
+      .addField("**By:**", "**[ " + `${message.author.tag}` + " ]**")
+      .addField("**Reason:**", "**[ " + `${reason}` + " ]**");
+    message.channel.send({
+      embed: kickembed
+    });
+  }
+});
+
+//كود افتار
+
+client.on("message", message => {
+  if (message.content.split(" ")[0] === prefix + "avt") {
+    if (message.author.bot || message.channel.type == "dm") return;
+    var args = message.content.split(" ")[1];
+    var avt = args || message.author.id;
+    client
+      .fetchUser(avt)
+      .then(user => {
+        avt = user;
+        let avtEmbed = new Discord.RichEmbed()
+          .setColor("#36393e")
+          .setAuthor(`${avt.username}'s Avatar`, message.author.avatarURL)
+          .setImage(avt.avatarURL)
+          .setFooter(`Avatar`, message.client.user.avatarURL);
+        message.channel.send(avtEmbed);
+      })
+      .catch(() => message.channel.send(`يجب عليك وضع ايدي الشخص`));
+  } 
+}); 
+
+////كود معلومات السيرفر
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "server")) {
+    if (!message.channel.guild)
+      return message.channel.send(` | This Command is used only in servers!`);
+    const millis = new Date().getTime() - message.guild.createdAt.getTime();
+    const now = new Date();
+    const verificationLevels = ["None", "Low", "Medium", "Insane", "Extreme"];
+    const days = millis / 1000 / 60 / 60 / 24;
+    var embed = new Discord.RichEmbed()
+      .setAuthor(message.guild.name, message.guild.iconURL)
+      .addField(":id:✽** Server ID:**", `» ${message.guild.id} `, true)
+      .addField(
+        ":calendar:✽** Created On**",
+        `» ${message.guild.createdAt.toLocaleString()}`,
+        true
+      )
+      .addField(":crown: ✽**Server Owner**", `**${message.guild.owner}**`, true)
+      .addField(
+        `✽** Members ** [${message.guild.members.size}]`,
+        `**${
+          message.guild.members.filter(c => c.presence.status !== "offline")
+            .size
+        }** **Online**`,
+        true
+      )
+      .addField(
+        ":speech_balloon:✽** Channels **",
+        `» **${message.guild.channels.filter(m => m.type === "text").size}**` +
+          " TexT | VoicE  " +
+          `**${message.guild.channels.filter(m => m.type === "voice").size}** `,
+        true
+      )
+      .addField(":earth_africa:✽** Region **", ` ${message.guild.region}`, true)
+      .setImage("")
+
+      .setColor("#000000");
+    message.channel.sendEmbed(embed);
+  }
+});
+
+//كود فك الميوت
+
+client.on("message", message => {
+  if (message.author.bot) return;
+
+  let command = message.content.split(" ")[0];
+
+  if (command === prefix + "unmute") {
+    if (message.author.bot) return;
+    if (!message.member.hasPermission("MANAGE_ROLES"))
+      return message
+        .reply("** لا يوجد لديك برمشن 'Manage Roles' **")
+        .catch(console.error);
+    let user = message.mentions.users.first();
+    let modlog = client.channels.find(gg => gg.name === "log");
+    let muteRole = client.guilds
+      .get(message.guild.id)
+      .roles.find(gg => gg.name === "Muted");
+    if (!muteRole)
+      return message
+        .reply("** لا يوجد لديك رتبه الميوت 'Muted' **")
+        .catch(console.error);
+    if (message.mentions.users.size < 1)
+      return message
+        .reply("** يجب عليك منشنت شخص اولاً**")
+        .catch(console.error);
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00ae86)
+      .setTimestamp()
+      .addField("الأستعمال:", "اسكت/احكي")
+      .addField(
+        "تم فك الميوت عن:",
+`${user.username}#${user.discriminator} (${user.id})`
+      )
+      .addField(
+        "بواسطة:",
+        `${message.author.username}#${message.author.discriminator}`
+      );
+
+    if (
+      !message.guild
+        .member(client.user)
+        .hasPermission("MANAGE_ROLES_OR_PERMISSIONS")
+    )
+      return message
+        .reply("** لا يوجد لدي برمشن Manage Roles **")
+        .catch(console.error);
+
+    if (message.guild.member(user).removeRole(muteRole.id)) {
+      return message
+        .reply("**:white_check_mark: .. تم فك الميوت عن الشخص **")
+        .catch(console.error);
+    } else {
+      message.guild
+        .member(user)
+        .removeRole(muteRole)
+        .then(() => {
+          return message
+            .reply("**:white_check_mark: .. تم فك الميوت عن الشخص **")
+            .catch(console.error);
+        });
+    }
+  }
+});
+
+        .catch(console.error);
+    if (message.mentions.users.size < 1)
+      return message
+        .reply("** يجب عليك منشنت شخص اولاً**")
+        .catch(console.error);
+
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00ae86)
+      .setTimestamp()
+      .addField("الأستعمال:", "اسكت/احكي")
+      .addField(
+        "تم ميوت:",
+        `${user.username}#${user.discriminator} (${user.id})`
+      )
+      .addField(
+        "بواسطة:",
+        `${message.author.username}#${message.author.discriminator}`
+      );
+
+    if (
+      !message.guild
+        .member(client.user)
+        .hasPermission("MANAGE_ROLES_OR_PERMISSIONS")
+    )
+      return message
+        .reply("** لا يوجد لدي برمشن Manage Roles **")
+        .catch(console.error);
+
+    if (message.guild.member(user).roles.has(muteRole.id)) {
+      return message
+        .reply("**:white_check_mark: .. تم اعطاء العضو ميوت**")
+        .catch(console.error);
+    } else {
+      message.guild
+        .member(user)
+        .addRole(muteRole)
+        .then(() => {
+          return message
+            .reply("**:white_check_mark: .. تم اعطاء العضو ميوت كتابي**")
+            .catch(console.error);
+        });
+    }
+  }
+});
+
+
+
+
 
 
 client.login("NzMxNzU1MDgwOTQzOTI3MzQ3.XwqqBg.SFbyU3eSM-iF3iUcH_0Hvcj7eLY");

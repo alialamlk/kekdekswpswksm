d44d7cafaed7ebd5bc3473bb6dd8ade48713d791
Glitch (hello-express) <none>
2020-10-07 07:37:10 +0000

    ⛺️👍 Checkpoint ./Alone.js:18921333/4

--- Alone.js
+++ Alone.js
@@ -6,7 +6,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://ksmk.glitch.me/`);
+  http.get(`http://.glitch.me/`);
 }, 280000);
 
 // كل البكجات الي ممكن تحتجها في اي بوت 

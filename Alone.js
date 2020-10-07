3576da080a598566462c0779f001ed41586c90e4
Glitch (hello-express) <none>
2020-10-07 08:37:19 +0000

    🍣🍁 Checkpoint ./Alone.js:18921333/24

--- Alone.js
+++ Alone.js
@@ -2528,7 +2528,7 @@ client.on("message", message => {
     )
       return message.channel.send(`You already have a ticket open.`);
     message.guild
-      .createChannel(`ticket-${message.author.id}`, "text")
+      .createChannel(`ticket-${x}`, "text")
       .then(c => {
         let role = message.guild.roles.find(gg => gg.name === "Support Team");
         let role2 = message.guild.roles.find(gg => gg.name === "@everyone");

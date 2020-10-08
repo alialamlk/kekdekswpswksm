6b561d2d9ef3ce226cdb48a83875f2f866594e69
Glitch (hello-express) <none>
2020-10-08 15:43:32 +0000

    🎮🎁 Checkpoint ./Alone.js:18921333/1558

--- Alone.js
+++ Alone.js
@@ -3177,7 +3177,87 @@ client.on(`ready`, ()=>{
 
 
 
+client.on("ready", function() {
+  var ms = 3500;
 
+  var setGame = [
+    "-help لروئيه الاوامر",
+    `${client.guilds.size} السيرفرات`,
+    `${client.users.size} الاعضاء`,
+    `-help`,
+    `احسن بوت في الدسكورد`,
+    `موجود كل شي في البوت `,
+    "انشر البوت حب"
+     
+  ];
+
+  var i = -1;
+
+  var j = 0;
+
+  setInterval(function() {
+    if (i == -1) {
+      j = 1;
+    }
+
+    if (i == setGame.length - 1) {
+      j = -1;
+    }
+
+    i = i + j;
+
+    client.user.setGame(setGame[i], `http://twitch.tv/S-F`);
+  }, ms);
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  console.log("وتف");
+
+  console.log("");
+
+  console.log(
+    "╔[═════════════════════════════════════════════════════════════════]╗"
+  );
+
+  console.log(`[Start] ${new Date()}`);
+
+  console.log(
+    "╚[═════════════════════════════════════════════════════════════════]╝"
+  );
+
+  console.log("");
+
+  console.log("╔[════════════════════════════════════]╗");
+
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+
+  console.log("");
+
+  console.log("Informations :");
+
+  console.log("");
+
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+
+  console.log(`Users! [ " ${client.users.size} " ]`);
+
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+
+  console.log("╚[════════════════════════════════════]╝");
+
+  console.log("");
+
+  console.log("╔[════════════]╗");
+
+  console.log(" Bot Is Online");
+
+  console.log("╚[════════════]╝");
+
+  console.log("");
+
+  console.log("");
+});
+ 
 
 
 
